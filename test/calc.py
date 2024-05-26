badee057f914cb9d993687dcc785860cfed37674
duma <126393227+vinzcyun@users.noreply.github.com>
2024-05-26 16:20:02 +0800

    Update calc.py

--- test/calc.py
+++ test/calc.py
@@ -7,7 +7,7 @@ from telegram.ext import CommandHandler, CallbackQueryHandler
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 logger = logging.getLogger(__name__)
 
-banner = "@yym68686"
+banner = "@vinzcyun"
 
 buttons = [
     [
